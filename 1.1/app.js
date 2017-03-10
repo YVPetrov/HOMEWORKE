a038bccf85f7f194a2a2864479c040da7c5584e0
YVPetrov <yv.petrov@physics.msu.ru>
2017-03-10 13:34:16 +0300

    Update app.js

--- 1.1/app.js
+++ 1.1/app.js
@@ -1,16 +1,6 @@
 var word1=prompt('Введите первое слово','');
 var word2=prompt('Введите второе слово','');
-if (word1.length == word1.length) {
-  // разбить строку на буквы, отсортировать
-  var sorted1 = word1.toLowerCase().split('').sort().join('');
-  var sorted2 = word2.toLowerCase().split('').sort().join(''); 
-  if (sorted1 == sorted2) {
-    alert('Анаграмма')
-  }
-  else{
-    alert('Не анаграмма')
-  }
-}
-else {
-  alert('Не анаграмма')
-}
+// разбить строку на буквы, отсортировать
+var sorted1 = word1.toLowerCase().split('').sort().join('');
+var sorted2 = word2.toLowerCase().split('').sort().join('');
+(sorted1 == sorted2) ? alert('Анаграмма') : alert('Не анаграмма');
