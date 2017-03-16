6e04e2b569b10a793cc523537655b490aa0f0f53
YVPetrov <yv.petrov@physics.msu.ru>
2017-03-16 21:56:09 +0300

    Create app.js

---
+++ 2.5/app.js
@@ -0,0 +1,11 @@
+function sum(x) {
+    return function(y) {
+        if (typeof y !== 'undefined') {
+            x = x + y;
+            return arguments.callee;
+        } else {
+            return x;
+        }
+    };
+}
+console.log(sum(3)(2)())
